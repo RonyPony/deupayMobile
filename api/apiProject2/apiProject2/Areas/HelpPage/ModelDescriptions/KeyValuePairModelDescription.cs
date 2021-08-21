60d99750567ca50ccffb84ae598a0298ba7f5012
ronel <ronel.cruz.a8@gmail.com>
2021-08-21 12:52:04 -0400

    moving to asp

---
+++ api/apiProject2/apiProject2/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace apiProject2.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
